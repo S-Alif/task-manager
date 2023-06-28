0a91912426c1ba440560c36fadb9de05fc0c28db
Alif <79266506+S-Alif@users.noreply.github.com>
2023-06-28 15:48:55 +0600

    Update: updated user model

--- backend/src/models/user-model.js
+++ backend/src/models/user-model.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 
 let user_schema = mongoose.Schema({
-  email: { type: String },
+  email: { type: String, unique: true},
   password: {type: String},
   firstName: {type: String},
   lastName: {type: String}
