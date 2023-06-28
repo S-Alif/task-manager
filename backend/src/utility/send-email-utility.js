0782629e158e1fa72e51b49e2bdd8120ade9f3b2
Alif <79266506+S-Alif@users.noreply.github.com>
2023-06-28 15:50:24 +0600

    Update: updated send-email utility

--- backend/src/utility/send-email-utility.js
+++ backend/src/utility/send-email-utility.js
@@ -2,28 +2,34 @@
 const nodemailer = require('nodemailer')
 const dotenv = require('dotenv')
 
-dotenv.config({path:'./secret-pass.env'})
+dotenv.config()
 
-exports.sendEmail = async (mailto, mailText, mailSubject) => {
-  let transporter = nodemailer.createTransport({
-    host: 'mail.teamrabbil.com',
-    port: 25,
-    secure: false,
-    auth:{
-      user: "info@teamrabbil.com",
-      pass: `${process.env.password}`
-    },
-    tls: {
-      rejectUnauthorized: false
+const sendEmail = async (mailto, mailText, mailSubject) => {
+  try {
+    let transporter = nodemailer.createTransport({
+      host: 'smtp-relay.brevo.com',
+      port: 587,
+      secure: false,
+      auth: {
+        user: `${process.env.mailUrl}`,
+        pass: `${process.env.mailPass}`
+      },
+      tls: {
+        rejectUnauthorized: false
+      }
+    })
+
+    let mailOption = {
+      from: "Task Manager Account Verification <sagor01md@gmail.com>",
+      to: mailto,
+      subject: mailSubject,
+      text: mailText
     }
-  })
 
-  let mailOption = {
-    from: "Task Manager MERN3<info@teamrabbil.com>",
-    to: mailto,
-    subject: mailSubject,
-    text: mailText
+    return await transporter.sendMail(mailOption)
+  } catch (error) {
+   console.log(error) 
   }
-
-  return await transporter.sendMail(mailOption)
 }
+
+module.exports = sendEmail
